8f1b2029e9c96f5005a2f25b57de24e6b35ca10c
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-18 17:29:00 -0500

    use apk add instead of apt-get for openssl

--- backend/nginx/generate-certs.sh
+++ backend/nginx/generate-certs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install openssl
-sudo apt-get install openssl
+apk add --no-cache openssl
 
 # Generate self-signed SSL certificate
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/nginx-selfsigned.key -out certs/nginx-selfsigned.crt -subj "/CN=localhost"
