c5500b182f57fb32f175cae97d754e12c3620cc5
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-18 00:17:58 -0500

    Update useSuggestions.js

--- frontend/src/components/useSuggestions.js
+++ frontend/src/components/useSuggestions.js
@@ -17,22 +17,22 @@ export default function useSuggestions(commanderName, count = 100) {
         return formattedName;
     };
 
-    useEffect(() => {
-        const fetchSuggestions = async () => {
-            setIsLoading(true);
-            try {
-                const response = await fetch(`${BASE_URL}api/${formatCommanderName(commanderName)}/suggestions/${count}`);
-                const data = await response.json();
-                setSuggestions(data.suggestions);
-            } catch (error) {
-                setError(error.message);
-            } finally {
-                setIsLoading(false);
-            }
-        };
+    const fetchSuggestions = async () => {
+        setIsLoading(true);
+        try {
+            const response = await fetch(`${BASE_URL}api/${formatCommanderName(commanderName)}/suggestions/${count}`);
+            const data = await response.json();
+            setSuggestions(data.suggestions);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    };
     
+    useEffect(() => {
         fetchSuggestions();
     }, [commanderName, count]);
 
-    return { suggestions, isLoading, error };
+    return { suggestions, isLoading, error, fetchSuggestions };
 }
