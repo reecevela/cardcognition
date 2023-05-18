80635f39f74ddbeca56395cfe7038045ab9dc050
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-18 17:34:50 -0500

    fix issue wwhere certs were being created in ./nginx instead of root

--- backend/nginx/generate-certs.sh
+++ backend/nginx/generate-certs.sh
@@ -3,5 +3,8 @@
 # install openssl
 apk add --no-cache openssl
 
+# Create the directory for certificates if it doesn't exist
+mkdir -p /etc/nginx/certs
+
 # Generate self-signed SSL certificate
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/nginx-selfsigned.key -out certs/nginx-selfsigned.crt -subj "/CN=localhost"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/nginx-selfsigned.key -out /etc/nginx/certs/nginx-selfsigned.crt -subj "/CN=localhost"
