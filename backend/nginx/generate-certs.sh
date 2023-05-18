8ca58ddf48a13b81ee638cea903330bab2a9fe39
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-18 17:27:12 -0500

    Installs openssl for nginx now

--- backend/nginx/generate-certs.sh
+++ backend/nginx/generate-certs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# install openssl
+sudo apt-get install openssl
+
 # Generate self-signed SSL certificate
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/nginx-selfsigned.key -out certs/nginx-selfsigned.crt -subj "/CN=localhost"
