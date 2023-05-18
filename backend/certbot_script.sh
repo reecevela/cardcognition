83c2421fc407d3ede46e0365763e848ef81a0dc9
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-18 16:25:02 -0500

    Add max tries to certbot script

--- backend/certbot_script.sh
+++ backend/certbot_script.sh
@@ -1,6 +1,15 @@
+# Max tries of 100
+
+MaxTries=100
+
 while ! nc -z backend_nginx_1 80; do   
   echo "nginx is unavailable - sleeping"
   sleep 1
+    ((MaxTries--))
+    if [ $MaxTries -le 0 ]; then
+        echo "Max tries reached"
+        exit 1
+    fi
 done
 
 echo "nginx is up - executing command"
