b6a550b3de8e05339e23119988aaefca5e505049
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-18 17:34:50 -0500

    fix issue wwhere certs were being created in ./nginx instead of root

--- backend/generate-certs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Generate self-signed SSL certificate
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/nginx-selfsigned.key -out certs/nginx-selfsigned.crt -subj "/CN=localhost"
