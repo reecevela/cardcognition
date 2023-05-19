62e7472d1ef7bb83ec95e6be435e78577d9d24d7
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-19 14:59:58 -0500

    Change useSuggestions to point to api.cardcognition.com

--- frontend/src/components/useSuggestions.js
+++ frontend/src/components/useSuggestions.js
@@ -5,7 +5,7 @@ export default function useSuggestions(commanderName, count = 100) {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    const BASE_URL = "https://cardcognition.com/";
+    const BASE_URL = "https://api.cardcognition.com";
 
     const formatCommanderName = (name) => {
         let formattedName = name.split(' // ')[0];
@@ -20,7 +20,7 @@ export default function useSuggestions(commanderName, count = 100) {
     const fetchSuggestions = async () => {
         setIsLoading(true);
         try {
-            const response = await fetch(`${BASE_URL}api/${formatCommanderName(commanderName)}/suggestions/${count}`);
+            const response = await fetch(`${BASE_URL}/${formatCommanderName(commanderName)}/suggestions/${count}`);
             const data = await response.json();
             setSuggestions(data.suggestions);
         } catch (error) {
