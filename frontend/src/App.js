5a39b5f6744f3b4722c1e6281c80cb5c744923d0
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-24 17:16:26 -0500

    Change layout, add footer, add og and metadata

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import DeckOptimizer from './components/DeckOptimizer';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <HeroSection />
       <DeckOptimizer />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            Copyright {
+                new Date().getFullYear()
+            } Reece Vela - All Rights Reserved
+        </footer>
+    )
+}
+
+export default Footer
