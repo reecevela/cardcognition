f0142b596eb62262ee0504c152d65d0af810e653
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-25 10:04:48 -0500

    Implement affiliate links

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,22 +3,31 @@ import { useEffect } from "react";
 
 function Card({ name, score, scryfall_id }) {
     const [imageUrl, setImageUrl] = React.useState("");
+    const [affiliateLink, setAffiliateLink] = React.useState("");
 
     useEffect(() => {
-        const fetchImage = async () => {
-            const imgResponse = await fetch(`https://api.scryfall.com/cards/${scryfall_id}`);
-            const imgData = await imgResponse.json();
+        const fetchScryfallData = async () => {
+            const response = await fetch(`https://api.scryfall.com/cards/${scryfall_id}`);
+            const data = await response.json();
+            const affiliateCode = "CARDCOGNITION";
 
-            setImageUrl(imgData.image_uris.normal);
+            setImageUrl(data.image_uris.normal);
+            setAffiliateLink(`https://www.tcgplayer.com/product/${data.tcgplayer_id}?utm_campaign=affiliate&utm_medium=${affiliateCode}&utm_source=${affiliateCode}`);
         };
 
-        fetchImage();
+        fetchScryfallData();
     }, [scryfall_id]);
+
+    const handleClick = () => {
+        // Open affiliate link in new tab
+        window.open(affiliateLink, "_blank");
+    };
 
     return (
         <div className="card">
             <p>{name} - {score}</p>
-            <img src={imageUrl} alt={name} />
+            <img src={imageUrl} alt={name} onClick={handleClick} />
+            <a href={affiliateLink} target="_blank" rel="noreferrer">Buy on TCGplayer</a>
         </div>
     );
 }
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,14 @@
+import tcgplayerLogo from "../images/TCGplayer-logo.png";
+
 function Footer() {
     return (
         <footer className="footer">
-            Copyright {
-                new Date().getFullYear()
-            } Reece Vela - All Rights Reserved
+            <p>
+                Copyright {
+                    new Date().getFullYear()
+                } Reece Vela - All Rights Reserved
+            </p>
+            <img src={tcgplayerLogo} alt="TCGplayer Logo" />
         </footer>
     )
 }
