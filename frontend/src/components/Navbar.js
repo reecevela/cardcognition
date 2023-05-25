108090920c21b82534a45242785bbc2b39bda2ee
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-25 12:39:15 -0500

    Create contact from page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,6 +7,7 @@ function Navbar() {
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/docs">Developers</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
     );
