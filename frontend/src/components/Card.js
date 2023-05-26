a35c2235d14d0d6bfea2ac09500c982a9248fa13
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-26 10:08:38 -0500

    Fix deck entry formatting issues

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,6 +5,7 @@ function Card({ name, score, scryfall_id }) {
     const [imageUrl, setImageUrl] = React.useState("");
     const [affiliateLink, setAffiliateLink] = React.useState("");
     const [cardPrice, setCardPrice] = React.useState(0);
+    const [foilCardPrice, setFoilCardPrice] = React.useState(0);
 
     useEffect(() => {
         const fetchScryfallData = async () => {
@@ -14,6 +15,7 @@ function Card({ name, score, scryfall_id }) {
 
             setImageUrl(data.image_uris.normal);
             setCardPrice(data.prices.usd);
+            setFoilCardPrice(data.prices.usd_foil);
             setAffiliateLink(`https://www.tcgplayer.com/product/${data.tcgplayer_id}?utm_campaign=affiliate&utm_medium=${affiliateCode}&utm_source=${affiliateCode}`);
         };
 
@@ -29,7 +31,7 @@ function Card({ name, score, scryfall_id }) {
         <div className="card">
             <p>{name} - {score}</p>
             <img src={imageUrl} alt={name} onClick={handleClick} />
-            <a href={affiliateLink} target="_blank" rel="noreferrer">Buy on TCGplayer - ${cardPrice}</a>
+            <a href={affiliateLink} target="_blank" rel="noreferrer">Buy on TCGplayer - ${(cardPrice !== null) ? cardPrice : foilCardPrice}</a>
         </div>
     );
 }
