b39711936dc6bfead3d203108ba3961181b2fa76
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-27 16:17:23 -0500

    Update developer docs

--- frontend/src/components/DevDocs.js
+++ frontend/src/components/DevDocs.js
@@ -16,12 +16,84 @@ function DevDocs() {
                     <li>Replace spaces with hyphens</li>
                     <li>Convert to lowercase</li>
                 </ol>
-                <p></p>
-                <p>For instance, "Atraxa, Praetor's Voice" should be formatted as "atraxa-praetors-voice".</p>
+                <ul>
+                    <li>"Atraxa, Praetor's Voice" =&gt; "atraxa-praetors-voice"</li>
+                    <li>"Esika God of the Tree // the Prismatic Bridge" =&gt; "esika-god-of-the-tree"</li>
+
+                </ul>
                 <p>The base URL for all API requests is <code>https://api.cardcognition.com</code>.</p>
             </header>
             <section>
                 <h2>API Endpoints</h2>
+                <h3>GET /dbinfo</h3>
+                <p>Returns the average synergy score (by card and by commander), the number of commanders, the number of card-commander pairs, and the number of unique cards in the database.</p>
+                <pre>
+                    <code> 
+                        {
+`{
+    "avg_commander_synergy_score": "6.4862930421185716",
+    "avg_synergy_score": "5.9592683877016941",
+    "card_commander_pairs_count": 454339,
+    "commander_count": 1706,
+    "unique_card_count": 18032
+}`
+                        }
+                    </code>
+                </pre>
+
+                <h3>GET /:formatted-commander-name/info</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Parameter</th>
+                            <th>Type</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>:formatted-commander-name</td>
+                            <td>String</td>
+                            <td>The name of the commander, properly formatted as outlined above.</td>
+                        </tr>
+                        <tr>
+                            <td>info</td>
+                            <td>Endpoint</td>
+                            <td>Endpoint that returns the commander's information including its average synergy score, the number of associated cards, and similar commanders.</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p>Example usage: <pre><code>https://api.cardcognition.com/urza-lord-high-artificer/info</code></pre></p>
+                <h4>Response Structure</h4>
+                <p>The API response is structured as a JSON object. The similar commanders are sorted by overlapping cards percentage in descending order.</p>
+                <pre>
+                    <code>
+                        {
+`{
+    "avg_synergy_score": "2.9328719723183391",
+    "card_count": 289,
+    "name": "urza-lord-high-artificer",
+    "scryfall_id": "9e7fb3c0-5159-4d1f-8490-ce4c9a60f567",
+    "similar_commanders": [
+        {
+            "name": "the-reality-chip",
+            "overlap_count": 207,
+            "overlap_percentage": "71.6262975778546713",
+            "scryfall_id": "d859de3a-0be1-4e66-b438-1c3d4ee756cd"
+        },
+        {
+            "name": "emry-lurker-of-the-loch",
+            "overlap_count": 202,
+            "overlap_percentage": "69.8961937716262976",
+            "scryfall_id": "20fec02d-77af-4975-b410-7097c7c28e7e"
+        },
+        ...
+    ]
+}`
+                        }
+                    </code>
+                </pre>
+
                 <h3>GET /:formatted-commander-name/suggestions/:count/</h3>
                 <table>
                     <thead>
@@ -49,7 +121,7 @@ function DevDocs() {
                         </tr>
                     </tbody>
                 </table>
-                <p>Example usage: <code>https://api.cardcognition.com/urza-lord-high-artificer/suggestions/3</code></p>
+                <p>Example usage: <pre><code>https://api.cardcognition.com/urza-lord-high-artificer/suggestions/3</code></pre></p>
                 <h4>Response Structure</h4>
                 <p>The API response is structured as a JSON object. Suggestions are sorted by score in descending order. In an upcoming update, a "next" key will be included for easy pagination.</p>
                 <pre>
