c06f89cde780b6b853b7e9663db53c1b3931f52f
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-28 17:49:02 -0500

    Add x in score to show it's a multiplier

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -29,7 +29,7 @@ function Card({ name, score, scryfall_id }) {
 
     return (
         <div className="card">
-            {(score) ? (<p>{name} - {score}</p>) : (<p>{name}</p>)}
+            {(score) ? (<p>{name} - {score}x</p>) : (<p>{name}</p>)}
             <img src={imageUrl} alt={name} onClick={handleClick} />
             <a href={affiliateLink} target="_blank" rel="noreferrer">Buy on TCGplayer - ${(cardPrice !== null) ? cardPrice : foilCardPrice}</a>
         </div>
